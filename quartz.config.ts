0ed46b60121d1aa45ef25e91e09625b1a50c8182
Paranjay Khachar <paranjay@Paranjays-MacBook-Air.local>
2025-07-13 03:51:49 +0530

    feat: update site configuration

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧠",
+    pageTitle: "🧠 IIT Madras Notes",
     pageTitleSuffix: " IIT Madras Notes",
     enableSPA: true,
     enablePopovers: true,
